10d961388ef75fc36e5e565329d75109055695ba
RRRR <robbinmathew@users.noreply.github.com>
2018-11-18 18:48:13 -0800

    Adding react-router-bootstrap package

--- setup.sh
+++ setup.sh
@@ -7,3 +7,7 @@ yarn add react-bootstrap
 # Source: https://serverless-stack.com/chapters/handle-routes-with-react-router.html
 # For supporting routes
 yarn add react-router-dom
+
+# Source: https://serverless-stack.com/chapters/adding-links-in-the-navbar.html
+# For preventing the full page to refresh
+yarn add react-router-bootstrap
