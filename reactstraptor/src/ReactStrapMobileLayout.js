e54a23a402155f811f00a547dc660d77544ecd14
robin <robbinmathew@users.noreply.github.com>
2019-01-09 00:07:30 -0800

    Adding a responsive grid to the mobile layout

--- reactstraptor/src/ReactStrapMobileLayout.js
+++ reactstraptor/src/ReactStrapMobileLayout.js
@@ -2,34 +2,90 @@ import React, { Component } from 'react';
 import { Container, Row, Col } from 'reactstrap';
 import { Card, CardImg, CardTitle, CardText, CardGroup, CardSubtitle, CardBody } from 'reactstrap';
 import {Button, Alert, Badge, ButtonGroup, ButtonDropdown, DropdownToggle, DropdownMenu, DropdownItem} from 'reactstrap';
+import { Media } from 'reactstrap';
 
 class ReactStrapMobileLayout extends Component {
+  renderMedia( bgColorClass ) {
+    return (
+      <Media list>
+        <Media tag="li" className={"rounded " + bgColorClass} >
+          <Media left href="#">
+            <Media object src="media_64x64.png" alt="Generic placeholder image" className="rounded-circle"/>
+          </Media>
+          <Media body>
+            <Media heading>
+              Media heading
+            </Media>
+            Cras sit all sikca most amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+          </Media>
+          {/*Media body done*/}
+        </Media>
+        {/*Media tag done*/}
+      </Media>
+    );
+  }
+
+  renderCard() {
+    return (
+      <Card>
+        <CardBody>
+          <CardTitle>My card title </CardTitle>
+        </CardBody>
+        <CardImg top width="100%" className="rounded"
+          src="https://placeholdit.imgix.net/~text?txtsize=33&txt=256%C3%97180&w=256&h=180" 
+           alt="Card image cap" />
+        <CardBody>
+          <CardTitle>March 35, 2122</CardTitle>
+          <CardTitle>Mars</CardTitle>
+
+          <ButtonGroup>
+            <Button color="primary" >One</Button>
+            <Button color="primary" >Two</Button>
+            <Button color="primary" >Three</Button>
+          </ButtonGroup>
+          <Alert color="secondary"> Alert link <a href="#" className="alert-link">click here</a> 
+            <Badge color="info" pill>pill badge</Badge>
+          </Alert>
+        </CardBody>
+        { this.renderMedia("bg-warning") }
+        { this.renderMedia("bg-success") }
+      </Card>
+    );
+  }
+
+  renderRow() {
+    /*
+      Extra small:  xs min-width=0
+      Small:        sm min-width=576
+      Medium:       md min-width=768
+      Large:        lg min-width=992
+      Extra Large:  xl min-width=1200
+      xs upto 0
+    */
+    return (
+      <Row>
+        <Col xs="0" sm="0" md="1" lg="0" xl="1">
+        </Col>
+        <Col xs="12" sm="12" md="10" lg="7" xl="6">
+        <CardGroup>
+          { this.renderCard() }
+        </CardGroup>
+        </Col>
+        <Col xs="0" sm="0" md="0" lg="4" xl="4">
+          { this.renderMedia("bg-danger") }
+          { this.renderMedia("bg-dark") }
+        </Col>
+        <Col xs="0" sm="0" md="1" lg="0" xl="1">
+        </Col>
+      </Row>
+    );
+  }
+
   render() {
     return (
-      <Container fluid flex flexCol justifyContent="center" className="bg-danger">
-        <Row fullHeight justifyContent="center" className="bg-danger">
-          <Col fullHeight className="bg-dark">
-            <Card
-                fullHeight
-                flexRow
-                alignItems="center"
-                style={{ overflowX: "auto" }}
-                body
-           >
-          <CardBody>
-           <CardTitle>Card title</CardTitle>
-          </CardBody>
-          <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=256%C3%97180&w=256&h=180" alt="Card image cap" />
-              <div>
-                <ul>
-                  <li>
-                  Its possible to write text here
-                  </li>
-                </ul>
-              </div>
-            </Card>
-          </Col>
-        </Row>
+      <Container fluid className="bg-light">
+        {this.renderRow() }
+        {this.renderRow() }
       </Container>
     );
   }
